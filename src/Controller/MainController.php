5b65d9d3330492d199896b17b36d437498987153
VinceSelva <vincenzo.selva@hotmail.fr>
2018-03-19 14:35:59 +0000

    zdq

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,19 +3,17 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route; //add this line to add usage of Route class.
+use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class MainController extends Controller
 {
-
-    /**
-     * @Route("/", name="app_home") //add this comment to annotations
-     * @Template("main/home.html.twig")
-     */
-    public function homeAction()
-    {
-		return ["project_name" => "Accueil"];
-    }
-
+	/**
+	* @Route("/", name="app_home")
+	* @Template("main/home.html.twig")
+	*/
+	public function homeAction()
+	{
+		return ["projetct_name" => "Accueil"];
+	}
 }
